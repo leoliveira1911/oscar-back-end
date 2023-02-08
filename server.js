45b45cb500fc2a3c7243d188f14380e4d7117821
leoliveira1911 <leoliveira1911@gmail.com>
2023-02-08 18:42:58 -0300

    CORS fixed 1.4

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 const PORT = 3002;
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const answer = { "Melhor Ator": 'Leonardo di Caprio', "Melhor Filme": 'Titanic', "Melhor Canção Original": 'Música4', "Melhor Efeitos Visuais": 'Música4', "Melhor Direção": 'Titanic' }
@@ -17,6 +17,12 @@ app.get('/', (req, res) => {
 })
 
 //Fixing CORS
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
