9544801c51be3a7941aae09d6df79d967d65f69e
leoliveira1911 <leoliveira1911@gmail.com>
2023-02-08 18:58:14 -0300

    CORS fixed 1.7

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader('Access-Control-Allow-Credentials', true);
   app.use(cors());
   next();
