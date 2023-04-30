d70f1a3ec3d21b80b26319cedeb1d26e4bb1c802
Leandro <leoliveira1911@gmail.com>
2023-04-30 15:41:41 -0300

    answer update

--- server.js
+++ server.js
@@ -8,17 +8,38 @@ const PORT = 3002;
 app.use(cors());
 app.use(express.json());
 
-const oscar = require('./new_oscar.json')
+const oscar = require("./new_oscar.json");
 
 const answer = {
-  "Actor in a Leading Role": 'Brendan Fraser', "Actor in a Supporting Role": 'Ke Huy Quan', "Actress in a Leading Role": 'Michelle Yeoh', "Actress in a Supporting Role": 'Jamie Lee Curtis', "Animated Feature Film": "Guillermo del Toro's Pinocchio", "Cinematography": 'All Quiet on the Western Front', "Costume Design": 'Black Panther: Wakanda Forever', "Directing": 'Everything Everywhere All at Once', "Documentary Feature Film": 'Navalny', "Documentary Short Film": 'The Elephant Whisperers', "Film Editing": 'Everything Everywhere All at Once', "International Feature Film": 'All Quiet on the Western Front', "Makeup and Hairstyling": 'The Whale', "Music (Original Score)": 'All Quiet on the Western Front', "Music (Original Song)": 'Naatu Naatu', "Best Picture": 'Everything Everywhere All at Once', "Production Design": 'All Quiet on the Western Front', "Short Film (Animated)": 'The Boy, the Mole, the Fox and the Horse', "Short Film (Live Action)": 'An Irish Goodbye', "Sound": 'Top Gun: Maverick', "Visual Effects": 'Avatar: The Way of Water', "Writing (Adapted Screenplay)": 'Women Talking', "Writing (Original Screenplay)": 'Everything Everywhere All at Once'
-}
+  "Actor in a Leading Role": "Brendan Fraser",
+  "Actor in a Supporting Role": "Ke Huy Quan",
+  "Actress in a Leading Role": "Michelle Yeoh",
+  "Actress in a Supporting Role": "Jamie Lee Curtis",
+  "Animated Feature Film": "Guillermo del Toro's Pinocchio",
+  "Cinematography": "All Quiet on the Western Front",
+  "Costume Design": "Black Panther: Wakanda Forever",
+  "Directing": "Everything Everywhere All at Once",
+  "Documentary Feature Film": "Navalny",
+  "Documentary Short Film": "The Elephant Whisperers",
+  "Film Editing": "Everything Everywhere All at Once",
+  "International Feature Film": "All Quiet on the Western Front",
+  "Makeup and Hairstyling": "The Whale",
+  "Music (Original Score)": "All Quiet on the Western Front",
+  "Music (Original Song)": "Naatu Naatu",
+  "Best Picture": "Everything Everywhere All at Once",
+  "Production Design": "All Quiet on the Western Front",
+  "Short Film (Animated)": "The Boy, the Mole, the Fox and the Horse",
+  "Short Film (Live Action)": "An Irish Goodbye",
+  "Sound": "Top Gun: Maverick",
+  "Visual Effects": "Avatar: The Way of Water",
+  "Writing (Adapted Screenplay)": "Women Talking",
+  "Writing (Original Screenplay)": "Everything Everywhere All at Once",
+};
 
-
-//teste 
-app.get('/', (req, res) => {
-  res.send('Funcionando OK')
-})
+//teste
+app.get("/", (req, res) => {
+  res.send("Funcionando OK");
+});
 
 //Fixing CORS
 // app.use(function (req, res, next) {
@@ -32,15 +53,15 @@ app.get('/', (req, res) => {
 
 // Route to get all votes OF ONE USER!
 app.get("/api/getVotes", (req, res) => {
-  console.log('função getVotes')
-  console.log('user: ')
-  const user = req.query.user
-  console.log(user)
+  console.log("função getVotes");
+  console.log("user: ");
+  const user = req.query.user;
+  console.log(user);
   db.query(`SELECT * FROM votes where user = '${user}'`, (err, result) => {
     if (err) {
       res.send(err);
     }
-    console.log('Função getVotes result:')
+    console.log("Função getVotes result:");
     console.log(result);
     res.send(result);
   });
@@ -53,8 +74,6 @@ app.get("/api/getVotes", (req, res) => {
 //   const nominee = req.body.nominee;
 //   const img = req.body.imgUrl
 //   const userName = req.body.userName
-
-
 
 //   console.log('função save: req.body:')
 //   console.log(req.body)
@@ -124,59 +143,55 @@ app.get("/api/getVotes", (req, res) => {
 //     res.status(404).send('VOTO INVÁLIDO')
 //   }
 
-
-
 // });
 //RANKING:
 
 //get all the  votes:
 async function getAllTheVotes() {
-  return new Promise(resolve => {
-
+  return new Promise((resolve) => {
     db.query(`SELECT * FROM votes`, (err, result) => {
       if (err) {
         return err;
       }
       resolve(result);
-    })
-  })
-
+    });
+  });
 }
 
 function countCorrectVotes(votes, answer) {
-  return Object.values(votes.reduce((acc, vote) => {
-    const user = vote.userName
-    const category = vote.category
-    const nominee = vote.nominee
+  return Object.values(
+    votes.reduce((acc, vote) => {
+      const user = vote.userName;
+      const category = vote.category;
+      const nominee = vote.nominee;
 
-    if (acc[user]) {
-      const correct = answer[category] === nominee ? acc[user].correct + 1 : acc[user].correct
-      acc[user] = { user: user, correct }
-      return acc
-    } else {
-      const correct = answer[category] === nominee ? 1 : 0
-      acc[user] = { user: user, correct }
-      console.log(acc)
-      return acc
-    }
-  }, {}))
+      if (acc[user]) {
+        const correct =
+          answer[category] === nominee
+            ? acc[user].correct + 1
+            : acc[user].correct;
+        acc[user] = { user: user, correct };
+        return acc;
+      } else {
+        const correct = answer[category] === nominee ? 1 : 0;
+        acc[user] = { user: user, correct };
+        console.log(acc);
+        return acc;
+      }
+    }, {})
+  );
 }
 
 function orderVotes(votes) {
-
-  const desc = (o1, o2) => o2.correct - o1.correct
-  return votes.sort(desc)
+  const desc = (o1, o2) => o2.correct - o1.correct;
+  return votes.sort(desc);
 }
-
-
-
 
 app.get("/api/getRanking", (req, res) => {
   getAllTheVotes()
-    .then(votes => countCorrectVotes(votes, answer))
-    .then(countedVotes => orderVotes(countedVotes))
-    .then(orderVotes => res.send(orderVotes))
-
+    .then((votes) => countCorrectVotes(votes, answer))
+    .then((countedVotes) => orderVotes(countedVotes))
+    .then((orderVotes) => res.send(orderVotes));
 });
 
 app.listen(PORT, () => {
