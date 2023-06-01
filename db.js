8478139df17fbae5c34154eae8a8356b3439bea5
Leandro <leoliveira1911@gmail.com>
2023-06-01 10:15:16 -0300

    bd server updated

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
-
-const db = mysql.createConnection({
-  host: "to-do-list-test.cmid9eadco8w.sa-east-1.rds.amazonaws.com",
-  database: "oscar",
-  port: 3306,
-  user: "admin",
-  password: "Oliveiraleo12.",
-});
-
+require("dotenv").config();
+// const db = mysql.createConnection({
+//   host: "to-do-list-test.cmid9eadco8w.sa-east-1.rds.amazonaws.com",
+//   database: "oscar",
+//   port: 3306,
+//   user: "admin",
+//   password: "Oliveiraleo12.",
+// });
+const db = mysql.createConnection(process.env.DATABASE_URL);
 module.exports = db;
--- testes.js
+++ testes.js
@@ -1,126 +1,42 @@
-const votes = [
-    {
-        id: 13,
-        user: 'Pf10m2UgKUU5wjAE4DVAzmfIjXp2',
-        category: 'Melhor Filme',
-        nominee: 'teste1'
-    },
-    {
-        id: 14,
-        user: 'Pf10m2UgKUU5wjAE4DVAzmfIjXp2',
-        category: 'Melhor Ator',
-        nominee: 'RDJ'
-    },
-    {
-        id: 15,
-        user: 'Pf10m2UgKUU5wjAE4DVAzmfIjXp2',
-        category: 'Melhor Atriz',
-        nominee: 'Atriz1'
-    },
-    {
-        id: 16,
-        user: 'Pf10m2UgKUU5wjAE4DVAzmfIjXp2',
-        category: 'Melhor Música',
-        nominee: 'Música4'
-    },
-    {
-        id: 17,
-        user: 'DG7K0bEvGeY0hp2FfKL1VRrkg3G3',
-        category: 'Melhor Filme',
-        nominee: 'Titanic'
-    },
-    {
-        id: 18,
-        user: 'DG7K0bEvGeY0hp2FfKL1VRrkg3G3',
-        category: 'Melhor Ator',
-        nominee: 'Leonardo di Caprio'
-    },
-    {
-        id: 19,
-        user: 'DG7K0bEvGeY0hp2FfKL1VRrkg3G3',
-        category: 'Melhor Atriz',
-        nominee: 'Atriz2'
-    },
-    {
-        id: 20,
-        user: 'DG7K0bEvGeY0hp2FfKL1VRrkg3G3',
-        category: 'Melhor Direção',
-        nominee: 'Titanic'
-    },
-    {
-        id: 21,
-        user: 'DG7K0bEvGeY0hp2FfKL1VRrkg3G3',
-        category: 'Melhor Roteiro Original',
-        nominee: 'Música1'
-    },
-    {
-        id: 22,
-        user: 'DG7K0bEvGeY0hp2FfKL1VRrkg3G3',
-        category: 'Melhor Maquiagem e Cabelo',
-        nominee: 'Música1'
-    },
-    {
-        id: 23,
-        user: 'DG7K0bEvGeY0hp2FfKL1VRrkg3G3',
-        category: 'Melhor Efeitos Visuais',
-        nominee: 'Música1'
-    },
-    {
-        id: 24,
-        user: 'Pf10m2UgKUU5wjAE4DVAzmfIjXp2',
-        category: 'Melhor Direção',
-        nominee: 'Avatar: O caminho da água'
-    },
-    {
-        id: 25,
-        user: 'l1VhsyGLmYPKZeccBJ0zHuQ9Pc72',
-        category: 'Melhor Filme',
-        nominee: 'Titanic'
-    },
-    {
-        id: 29,
-        user: 'Pf10m2UgKUU5wjAE4DVAzmfIjXp2',
-        category: 'Melhor Efeitos Visuais',
-        nominee: 'Música4'
-    }
-]
+const oscar = require("./new_oscar.json");
+const db = require("./db");
 
-answer = { "Melhor Ator": 'Leonardo di Caprio', "Melhor Filme": 'Titanic', "Melhor Música": 'Música4', "Melhor Efeitos Visuais": 'Música4', "Melhor Direção": 'Titanic' }
+const nominees = oscar.flatMap((category) => {
+  return category.nominees.map((nominee) => {
+    const nom = nominee.title;
+    const img = nominee.imageUrl;
+    return { nom, img };
+  });
+});
+console.log(nominees);
+const uniqueNominees = nominees.filter(function (a) {
+  return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
+}, Object.create(null));
+console.log(nominees.length, uniqueNominees.length);
 
-function organizeVotes(votes, answer) {
-    return Object.values(votes.reduce((acc, vote) => {
-        const user = vote.user
-        const category = vote.category
-        const nominee = vote.nominee
+console.log(uniqueNominees.length);
+const insertingNominees = () =>
+  uniqueNominees.forEach((nominee) => {
+    return db.query(
+      `INSERT INTO nominees (nominee, img)
+       VALUES("${nominee.nom}" , "${nominee.img}")`
+    );
+  });
 
-        if (acc[user]) {
-            const correct = answer[category] === nominee ? acc[user].correct + 1 : acc[user].correct
-            acc[user] = { user: user, correct }
-            return acc
-        } else {
-            const correct = answer[category] === nominee ? 1 : 0
-            acc[user] = { user: user, correct }
-            console.log(acc)
-            return acc
-        }
-    }, {}))
+insertingNominees();
 
+const categories = oscar.map((category) => {
+  return category.name;
+});
 
-
-    // function joinRepeatedWords(arrayOfStrings) {
-    //     return Object.values(arrayOfStrings.reduce((acc, word) => {
-    //         const w = word.toLowerCase()
-    //         const qtde = acc[w] ? acc[w].qtde + 1 : 1
-    //         acc[w] = { word: w, qtde }
-
-    //         return acc
-    //     }, {}))
-    // }
-
-}
-
-
-
-
-// console.log(organizeVotes(votes))
-console.log(organizeVotes(votes, answer))
+const insertingCategories = () =>
+  categories.map((category) => {
+    return db.query(
+      `INSERT INTO category (category)
+     VALUES("${category}")`
+    );
+  });
+//insertingCategories();
+console.log(categories);
+console.log("Acabou");
+console.log(db.query("SELECT * FROM nominees"));
