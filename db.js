aaeed8ff76e777621c89eca33e3a3eac24c462b0
Leandro <leoliveira1911@gmail.com>
2023-06-01 10:21:06 -0300

    changed db server

--- db.js
+++ db.js
@@ -1,12 +1,6 @@
 const mysql = require("mysql");
 require("dotenv/config");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 module.exports = db;
